222ca2938130472648ef13c4aecda34c4bc85caa
raweber <raweber@student.42wolfsburg.de>
2023-07-09 18:42:35 +0200

    Add requirements for project

--- src/main.c
+++ src/main.c
@@ -1,6 +1,17 @@
 #include <unistd.h>
 #include <stdio.h>
 
-int main (void) {
-  printf("hello\n");
+int main (int args, char **argv) {
+
+  
+  // check if its a valid IP address
+  // invalid number as ip -> "ping: 8.8.8.88888: Name or service not known"
+  // invalid dns -> "ping: google: No address associated with hostname"
+  // if valid ip/dns: "PING 8.8.8.123 (8.8.8.123) 56(84) bytes of data."
+
+
+
+
+  // loop the ping / count the pings for -c flag if set
+  // sends packet every second, endlessly by default
 }
